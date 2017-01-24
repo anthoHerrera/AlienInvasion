872d4735f5c645f513d4af6655c99b82cad6cdf6
USER <USER@AnthoHerrera>
2017-01-24 17:17:55 -0500

    first view

--- src/invasionalien/PaneOrganizer.java
+++ src/invasionalien/PaneOrganizer.java
@@ -42,64 +42,11 @@ public class PaneOrganizer {
         //naveAlien = new AlienShip(paneScreen);
         //player = new Jugador(paneScreen);
         //root.setCenter(paneScreen);
-        InputStream is = Files.newInputStream(Paths.get("Fondos/alieninvasion.png"));
-        BackgroundImage imm = new BackgroundImage(new Image(is,640,500, false, true), BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, BackgroundSize.DEFAULT);
-        root.setBackground(new Background(imm));
-        this.setupPane();
    
     }
 
     public Pane getRoot() {
         return root;
     }
-
-    
-    private void setupPane(){
-        Label titulo = new Label("Alien invasion!");
-        Button jugarBtn = new Button("Jugar");
-        Button intruccioneBtn = new Button("Instrucciones");
-        Button scoreBtn = new Button("Scores");
-        jugarBtn.setPrefSize(100, 30);
-        intruccioneBtn.setPrefSize(100, 30);
-        scoreBtn.setPrefSize(100, 30);
-        titulo.setTextFill(Color.GREENYELLOW);
-        titulo.setFont(new Font("Harrington", 40));
-        titulo.setStyle("");
-        intruccioneBtn.setOnAction(new instructionHandler());
-        root.getChildren().addAll(titulo,jugarBtn,intruccioneBtn,scoreBtn);
-    }
-    
-    private class instructionHandler implements EventHandler<ActionEvent> {
-        @Override
-        public void handle(ActionEvent event) {
-            
-            TextArea instructions = new TextArea();
-            String instrucciones = "El jugador tiene un numero limitado de misiles por nivel (10 por cada nivel)\n" +
-                    "Con respecto a las teclas, M permitirá disparar los misiles y con barra espaciadora disparo las balas.\n" +
-                    "Con P puedo pausar el juego.\n" +
-                    "Con E empiezo el juego.\n" +
-                    "Con las teclas <- -> me muevo de derecha a izquierda\n" +
-                    "Con la tecla Escape se sale del juego.";
-            instructions.setText(instrucciones);
-            instructions.setFont(new Font("times new roman", 16));
-            Button volverBtn = new Button("Volver");
-            volverBtn.setOnAction(new EventHandler<ActionEvent>() {
-                @Override
-                public void handle(ActionEvent event) {
-                    for(Node n: root.getChildren()) {
-                        n.setVisible(true);
-                    }
-                    root.getChildren().remove(instructions);
-                    root.getChildren().remove(volverBtn);
-                    
-                }
-            });
-            for(Node n: root.getChildren()) {
-                n.setVisible(false);
-            }
-            root.getChildren().addAll(instructions,volverBtn);
-            
-        }
-        
-    }
+  
 }

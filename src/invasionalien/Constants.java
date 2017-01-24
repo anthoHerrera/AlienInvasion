f99fcde5e71722b3d7ed81dc626c2b1efa7d6bb4
USER <USER@AnthoHerrera>
2017-01-24 18:30:56 -0500

    new view

--- src/invasionalien/Constants.java
+++ src/invasionalien/Constants.java
@@ -10,30 +10,26 @@ package invasionalien;
  * @author USER
  */
 public class Constants {
+    //app
     public static final double APP_WIDHT = 640;
     public static final double APP_HEIGHT = 500;
+    //Alien ship
     public static final double X_ALIEN_SHIP = 15;
     public static final double Y_ALIEN_SHIP = 10;
     public static final double X_CABINA = 5;
     public static final double Y_CABINA = 3;
     public static final double WIDHT_ALA_AS= 5;
     public static final double HEIGHT_ALA_AS = 30;
-    public static final double X_HUMAN_SHIP = 10;
+    //Human ship
+    public static final double X_HUMAN_SHIP = 12;
     public static final double Y_HUMAN_SHIP = 16;
-    public static final double X1_MOTOR_IZQ = 52;
-    public static final double Y1_MOTOR_IZQ = 50;
-    public static final double X2_MOTOR_IZQ = 62;
-    public static final double Y2_MOTOR_IZQ = 50;
-    public static final double X3_MOTOR_IZQ = 57;
-    public static final double Y3_MOTOR_IZQ = 30;
-    public static final double X1_MOTOR_DER = 6;
-    public static final double Y1_MOTOR_DER = 50;
-    public static final double X2_MOTOR_DER = 16;
-    public static final double Y2_MOTOR_DER = 50;
-    public static final double X3_MOTOR_DER = 11;
-    public static final double Y3_MOTOR_DER = 30;
-   
-    
-   
+    public static final double X1_MOTOR = 10;
+    public static final double Y1_MOTOR = 30;
+    public static final double X2_MOTOR = 20;
+    public static final double Y2_MOTOR = 30;
+    public static final double X3_MOTOR = 15;
+    public static final double Y3_MOTOR = 15;
+    public static final double X_OFFSET = 325;
+    public static final double Y_OFFSET = 450;
     
 }
